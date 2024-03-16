3cbdf39de88ac4f15434370caa3793be1666be10
ᴛʜᴇ ᴢᴇᴇᴅ <octokardlooy@gmx.us>
2024-03-17 00:21:10 +0300

    Update enough.py

--- set/enough.py
+++ set/enough.py
@@ -9,7 +9,7 @@ for attribute in dir(SendSms):
     attribute_value = getattr(SendSms, attribute)
     if callable(attribute_value):
         if attribute.startswith('__') == False:
-            servisler_sms.append(attribute)
+            servisler_service.append(attribute)
 
             
 while 1:
@@ -25,7 +25,7 @@ while 1:
                                  
     
     Sms: {}           {}by {}@tingirifistik\n  
-    """.format(Fore.LIGHTCYAN_EX, len(servisler_service), Style.RESET_ALL, Fore.LIGHTRED_EX))
+    """.format(Fore.LIGHTCYAN_EX, len(servisler_service.py), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
         if menu == "":
@@ -159,55 +159,7 @@ while 1:
             while True:
                 with ThreadPoolExecutor() as executor:
                     futures = [
-                        executor.submit(send_sms.Akasya),
-                        executor.submit(send_sms.Akbati),
-                        executor.submit(send_sms.Ayyildiz),
-                        executor.submit(send_sms.Baydoner),
-                        executor.submit(send_sms.Beefull),
-                        executor.submit(send_sms.Bim),
-                        executor.submit(send_sms.Bisu),
-                        executor.submit(send_sms.Bodrum),
-                        executor.submit(send_sms.Clickme),
-                        executor.submit(send_sms.Dominos),
-                        executor.submit(send_sms.Englishhome),
-                        executor.submit(send_sms.Evidea),
-                        executor.submit(send_sms.File),
-                        executor.submit(send_sms.Frink),
-                        executor.submit(send_sms.Happy),
-                        executor.submit(send_sms.Hayatsu),
-                        executor.submit(send_sms.Hey),
-                        executor.submit(send_sms.Hizliecza),
-                        executor.submit(send_sms.Icq),
-                        executor.submit(send_sms.Ipragaz),
-                        executor.submit(send_sms.Istegelsin),
-                        executor.submit(send_sms.Joker),
-                        executor.submit(send_sms.KahveDunyasi),
-                        executor.submit(send_sms.KimGb),
-                        executor.submit(send_sms.Komagene),
-                        executor.submit(send_sms.Koton),
-                        executor.submit(send_sms.KuryemGelsin),
-                        executor.submit(send_sms.Macro),
-                        executor.submit(send_sms.Metro),
-                        executor.submit(send_sms.Migros),
-                        executor.submit(send_sms.Naosstars),
-                        executor.submit(send_sms.Paybol),
-                        executor.submit(send_sms.Pidem),
-                        executor.submit(send_sms.Porty),
-                        executor.submit(send_sms.Qumpara),
-                        executor.submit(send_sms.Starbucks),
-                        executor.submit(send_sms.Suiste),
-                        executor.submit(send_sms.Taksim),
-                        executor.submit(send_sms.Tasdelen),
-                        executor.submit(send_sms.Tasimacim),
-                        executor.submit(send_sms.Tazi),
-                        executor.submit(send_sms.TiklaGelsin),
-                        executor.submit(send_sms.ToptanTeslim),
-                        executor.submit(send_sms.Ucdortbes),
-                        executor.submit(send_sms.Uysal),
-                        executor.submit(send_sms.Wmf),
-                        executor.submit(send_sms.Yapp),
-                        executor.submit(send_sms.YilmazTicaret),
-                        executor.submit(send_sms.Yuffi)
+                        executor.submit(send_service.py),
                     ]
                     wait(futures)
         except KeyboardInterrupt:
